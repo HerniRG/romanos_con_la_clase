fbd530ead51ae4ffb87482fc42f8dcf6093f2078
HernánRG <hernanrg85@gmail.com>
2024-07-02 16:50:44 +0200

    to_roman completo

--- roman_funcs.py
+++ roman_funcs.py
@@ -31,12 +31,36 @@ arabic_to_roman = {
     1: 'I'
 }
 
-def to_roman(n):
-    arab = n
-    roman = ""
-    for key, value in arabic_to_roman.items():
-        while arab >= key:
-            roman += value
-            arab -= key
-    return roman
+def to_roman_9_or_more(n):
+    result = "" 
+    multiplicador = 10 ** (len(str(n)) - 1)  # si n es 850, len seria 3, 3-1 = 2, 10 * 10 = 100
+    
+    while n > 0:  # Bucle para procesar cada dígito del número
+        digit = n // multiplicador  # Obtiene el dígito más grande 850 // 100 = 8
+        n = n % multiplicador  # Actualiza el número eliminando más grande 850 % 100 = 50
+        
+        
+        if digit <= 3:
+            result += digit * arabic_to_roman[multiplicador]
+        elif digit == 4:
+            result += arabic_to_roman[multiplicador] + arabic_to_roman[5 * multiplicador]
+        elif digit < 9:
+            result += arabic_to_roman[5 * multiplicador] + (digit - 5) * arabic_to_roman[multiplicador]
+        elif digit == 9:
+            result += arabic_to_roman[multiplicador] + arabic_to_roman[10 * multiplicador]
+        
+        multiplicador = multiplicador // 10 # Reduce el multiplicador 100 // 10 = 10
+
+    return result
+
+to_roman_9_or_more(890)
+
+# def to_roman(n):
+#     arab = n
+#     roman = ""
+#     for key, value in arabic_to_roman.items():
+#         while arab >= key:
+#             roman += value
+#             arab -= key
+#     return roman
             
--- test_roman_number.py
+++ test_roman_number.py
@@ -1,11 +1,57 @@
-from roman_funcs import to_roman
+from roman_funcs import to_roman_9_or_more
 
 
 def test_romanos_simples():
-    assert to_roman(1) == "I"
-    assert to_roman(5) == "V"
-    assert to_roman(10) == "X"
-    assert to_roman(50) == "L"
-    assert to_roman(100) == "C"
-    assert to_roman(500) == "D"
-    assert to_roman(1000) == "M"
+    assert to_roman_9_or_more(1) == "I"
+    assert to_roman_9_or_more(5) == "V"
+    assert to_roman_9_or_more(10) == "X"
+    assert to_roman_9_or_more(50) == "L"
+    assert to_roman_9_or_more(100) == "C"
+    assert to_roman_9_or_more(500) == "D"
+    assert to_roman_9_or_more(1000) == "M"
+
+def test_romanos_complejos():
+    assert to_roman_9_or_more(4) == "IV"
+    assert to_roman_9_or_more(6) == "VI"
+    assert to_roman_9_or_more(9) == "IX"
+    assert to_roman_9_or_more(21) == "XXI"
+    assert to_roman_9_or_more(42) == "XLII"
+    assert to_roman_9_or_more(78) == "LXXVIII"
+    assert to_roman_9_or_more(99) == "XCIX"
+    assert to_roman_9_or_more(125) == "CXXV"
+    assert to_roman_9_or_more(444) == "CDXLIV"
+    assert to_roman_9_or_more(890) == "DCCCXC"
+    assert to_roman_9_or_more(999) == "CMXCIX"
+
+from roman_funcs import to_roman_9_or_more
+
+def test_romanos_simples():
+    assert to_roman_9_or_more(1) == 'I'
+    assert to_roman_9_or_more(5) == 'V'
+    assert to_roman_9_or_more(10) == 'X'
+    assert to_roman_9_or_more(50) == 'L'
+    assert to_roman_9_or_more(100) == 'C'
+    assert to_roman_9_or_more(500) == 'D'
+    assert to_roman_9_or_more(1000) == 'M'
+
+def test_romanos_1_al_9():
+    assert to_roman_9_or_more(1) == 'I'
+    assert to_roman_9_or_more(2) == 'II'
+    assert to_roman_9_or_more(3) == 'III'
+    assert to_roman_9_or_more(4) == 'IV'
+    assert to_roman_9_or_more(5) == 'V'
+    assert to_roman_9_or_more(6) == "VI"
+    assert to_roman_9_or_more(7) == 'VII'
+    assert to_roman_9_or_more(8) == 'VIII'
+    assert to_roman_9_or_more(9) == 'IX'
+
+def test_romanos_10_al_90():
+    assert to_roman_9_or_more(10) == 'X'
+    assert to_roman_9_or_more(20) == 'XX'
+    assert to_roman_9_or_more(30) == 'XXX'
+    assert to_roman_9_or_more(40) == 'XL'
+    assert to_roman_9_or_more(50) == 'L'
+    assert to_roman_9_or_more(60) == "LX"
+    assert to_roman_9_or_more(70) == 'LXX'
+    assert to_roman_9_or_more(80) == 'LXXX'
+    assert to_roman_9_or_more(90) == 'XC'
